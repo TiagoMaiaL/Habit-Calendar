e752544f5da0f26531b42a88a4755560b8cc2c8e
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-06-01 08:38:20 -0300

    Adding model entity files.

---
+++ Active/Active/Model/Day.swift
@@ -0,0 +1,13 @@
+//
+//  Day.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 01/06/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import CoreData
+
+class Day: NSManagedObject {
+
+}
---
+++ Active/Active/Model/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 01/06/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import CoreData
+
+class Notification: NSManagedObject {
+
+}
