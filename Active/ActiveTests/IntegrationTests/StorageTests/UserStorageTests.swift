6481a29e2c65b243c733af857c9513d9652ce59a
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-06-27 21:40:11 -0300

    UserStorageTests: refactoring tests to use the dummy factory.

--- Active/ActiveTests/IntegrationTests/StorageTests/UserStorageTests.swift
+++ Active/ActiveTests/IntegrationTests/StorageTests/UserStorageTests.swift
@@ -37,42 +37,59 @@ class UserStorageTests: IntegrationTestCase {
 
     func testUserCreation() {
         let user = userStorage.create()
-        XCTAssertNotNil(user.created, "User creation.")
-        XCTAssertNotNil(user.id, "User should have an id.")
+        XCTAssertNotNil(
+            user.created,
+            "The user created property should be declared."
+        )
+        XCTAssertNotNil(
+            user.id,
+            "User should have an id."
+        )
     }
     
     func testUserRetrieval() {
         var user = userStorage.getUser()
         
         // Since there isn't a created user, it should return nil.
-        XCTAssertNil(user, "No user shoud be fetched, since no user was created.")
+        XCTAssertNil(
+            user,
+            "No user shoud be fetched, since no user was created."
+        )
         
-        // Create a new user and Hold the userId to compare
-        // with the retrieved user's one.
-        let userId = userStorage.create().id
+        // Generate an user dummy and Hold the userId to compare
+        // with the retrieved one.
+        let userId = factories.user.makeDummy().id
         
         // Get the previously created user.
         user = userStorage.getUser()
-        // Get the retrieved user's id for comparision purposes.
-        let fetchedUserId: String? = user?.id
         
         // The previously created user should now be returned:
-        XCTAssertNotNil(user, "A user should be fetched from the getUser() method.")
+        XCTAssertNotNil(
+            user,
+            "A user should be fetched from the getUser() method."
+        )
+        
         // Check if the retrieved user is the same
         // as the previously created one.
-        XCTAssertEqual(userId, fetchedUserId, "The fetched user's id should be equal to the id of the previously created user.")
+        XCTAssertEqual(
+            user?.id,
+            userId,
+            "The fetched user's id should be equal to the id of the previously created user."
+        )
     }
     
     func testUserDeletion() {
-        // Create the user to be deleted.
-        _ = userStorage.create()
+        // Create a dummy user to be deleted.
+        _ = factories.user.makeDummy()
+        
         // Delete the user.
         userStorage.delete()
         // Check the return of the fetch method.
         let user = userStorage.getUser()
         
-        XCTAssertNil(user ?? nil, "The created user should be deleted.")
+        XCTAssertNil(
+            user,
+            "The created user should be deleted."
+        )
     }
-    
-    // TODO: Consider adding a new test to test the relationship with Habit entities.
 }
