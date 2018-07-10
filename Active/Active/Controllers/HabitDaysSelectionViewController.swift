452ef9c65f1d9a07595ff6cb110f4419e920860e
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-07-10 11:46:33 -0300

    Adding cocoaPods to the project. Adding JTAppleCalendar.

--- Active/Active/Controllers/HabitDaysSelectionViewController.swift
+++ Active/Active/Controllers/HabitDaysSelectionViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 /// The controller used to select the days in which the habit is
 /// going to be tracked.
 class HabitDaysSelectionViewController: UIViewController {
 
     // MARK: Properties
+    
+    // The cell's reusable identifier.
+    private let cellIdentifier = "day collection view cell"
 
-    /// The picker view used to select the day.
-    @IBOutlet weak var dayPickerView: UIDatePicker!
+    /// The calendar view with the days to be selected.
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
     
     /// The button the user uses to tell when he's done.
     @IBOutlet weak var doneButton: UIButton!
@@ -27,17 +31,58 @@ class HabitDaysSelectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure the calendar view.
+        calendarView.calendarDelegate = self
+        calendarView.calendarDataSource = self
     }
     
     // MARK: Actions
     
     @IBAction func selectDay(_ sender: UIButton) {
-        // Get the picker's date and pass it to the delegate.
-        let dayDate = dayPickerView.date
-        delegate?.didSelectDays([dayDate])
+        // TODO: Pass the selected array of dates.
         
         // Pop the current controller.
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension HabitDaysSelectionViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    
+    // MARK: JTAppleCalendarViewDataSource Methods
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(
+            startDate: startDate,
+            endDate: endDate,
+            numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .sunday
+        )
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(
+            withReuseIdentifier: cellIdentifier,
+            for: indexPath
+        ) as! CalendarDayCell
+     
+        // Configure the cell's UI.
+        cell.dayTitleLabel.text = cellState.text
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        print("=X")
     }
     
 }
---
+++ Active/Active/Views/CalendarDayCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarDayCell.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 09/07/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import JTAppleCalendar
+
+class CalendarDayCell: JTAppleCell {
+    
+    // MARK: Parameters
+    
+    /// The day's title label.
+    @IBOutlet weak var dayTitleLabel: UILabel!
+    
+}
