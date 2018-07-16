1aee26771438669b9d17667ed92b094e39fcbec2
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-07-16 12:07:20 -0300

    Adding new entities: OffensiveMO and DaysSequenceMO. Both to track the days and offensive features of the app.

---
+++ Active/Active/Model/DaysSequenceMO.swift
@@ -0,0 +1,13 @@
+//
+//  DaysSequenceMO.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 16/07/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import CoreData
+
+/// A sequence of n days a User has setup for an specific Habit entity to be
+/// tracked and executed on.
+class DaysSequenceMO: NSManagedObject {}
---
+++ Active/Active/Model/OffensiveMO.swift
@@ -0,0 +1,15 @@
+//
+//  OffensiveMO.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 16/07/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import CoreData
+
+/// The offensives a user has accomplished for a given habit.
+/// - Note: Offensives are created whenever the user executes a habit on an
+/// specific day and they continue until the user breaks a sequence of
+/// consecutive days.
+class OffensiveMO: NSManagedObject {}
