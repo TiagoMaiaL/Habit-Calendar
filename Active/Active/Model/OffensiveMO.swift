b1a41f9ecae0fff133eee658d140fcdb5f7a1216
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-07-19 14:03:09 -0300

    OffensiveMO: Getting offensive's length.

--- Active/Active/Model/OffensiveMO.swift
+++ Active/Active/Model/OffensiveMO.swift
@@ -12,4 +12,24 @@ import CoreData
 /// - Note: Offensives are created whenever the user executes a habit on an
 /// specific day and they continue until the user breaks a sequence of
 /// consecutive days.
-class OffensiveMO: NSManagedObject {}
+class OffensiveMO: NSManagedObject {
+    
+    // MARK: Imperatives
+    
+    /// Calculates the offensive's length
+    /// (diffence between its boundary dates).
+    /// - Returns: The offensive's length.
+    func getLength() -> Int {
+        // Get its from and to dates.
+        guard let fromDate = fromDate, let toDate = toDate else {
+            assertionFailure(
+                "Inconsistency: Both the from and to dates must be set."
+            )
+            return 0
+        }
+        
+        // Get the difference.
+        let difference = fromDate.getDifferenceInDays(from: toDate)
+        return difference > 0 ? difference : 1
+    }
+}
