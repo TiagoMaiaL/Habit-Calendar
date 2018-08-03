0b29c7421a4abead189ea17521e92051d84135f9
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-08-03 19:12:29 -0300

    CardView: Adding a card view with inspectable attributes.

--- Active/Active/Views/CardView.swift
+++ Active/Active/Views/CardView.swift
@@ -7,11 +7,69 @@
 //
 
 import UIKit
+import QuartzCore
 
 /// A view used to contain other views in a card based design.
-class CardView: UIView {
+@IBDesignable class CardView: UIView {
 
     // MARK: Properties
     
-    // TODO:
+    // TODO: Add the shadow.
+    
+    /// The card's corner radius.
+    @IBInspectable public var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    /// The card's shadow's color.
+    @IBInspectable public var shadowColor: UIColor = .black
+    
+    /// The card's shadow's offset.
+    @IBInspectable public var shadowOffset: CGSize = CGSize(width: -15, height: 20)
+    
+    /// The card's shadow's radius.
+    @IBInspectable public var shadowRadius: CGFloat = 5
+    
+    /// The card's shadow's opacity.
+    @IBInspectable public var shadowOpacity: Float = 0.5
+    
+    // MARK: Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupShadow()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupShadow()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupShadow()
+    }
+    
+    // MARK: Life cycle
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupShadow()
+    }
+    
+    // MARK: Imperatives
+    
+    /// Configures the card's shadow according to the inspectable properties.
+    private func setupShadow() {
+        layer.masksToBounds = false
+        layer.shadowRadius = shadowRadius
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowOffset = shadowOffset
+        layer.shadowColor = shadowColor.cgColor
+    }
 }
