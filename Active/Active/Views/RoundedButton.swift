f0c4fe6459590105d3d272579d42b1f0e80975a9
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-08-07 15:15:36 -0300

    HabitCreationTableViewController: Changing done button's layout.

---
+++ Active/Active/Views/RoundedButton.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedButton.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 07/08/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+/// A custom rounded button.
+@IBDesignable class RoundedButton: UIButton {
+    
+    // MARK: Properties
+    
+    override var isEnabled: Bool {
+        didSet {
+            // Set the button's alpha to show if it's enabled or not.
+            UIViewPropertyAnimator(duration: 0.5, curve: .easeOut) {
+                self.alpha = (self.isEnabled ? 1 : 0.5)
+            }.startAnimation()
+        }
+    }
+    
+    // MARK: Life Cycle
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        layoutIfNeeded()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Configure the button's corner radius.
+        layer.cornerRadius = frame.size.height / 2
+    }
+
+}
