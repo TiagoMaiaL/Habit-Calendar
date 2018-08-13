70e8d6f7e9476dc2335513c315c2b5187d37e952
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-08-13 12:36:47 -0300

    Display the picker colors in circles

--- Active/Active/Views/ColorsPickerView/ColorOptionCollectionViewCell.swift
+++ Active/Active/Views/ColorsPickerView/ColorOptionCollectionViewCell.swift
@@ -16,7 +16,34 @@ class ColorOptionCollectionViewCell: UICollectionViewCell {
     /// The color being displayed by the cell.
     var optionColor: UIColor? {
         didSet {
-            backgroundColor = optionColor
+            colorView.backgroundColor = optionColor
         }
+    }
+
+    /// The rouded view to display the color option.
+    private(set) lazy var colorView = UIView()
+
+    /// MARK: Life Cycle
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if colorView.superview == nil {
+            colorView.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(colorView)
+        }
+
+        // Center the colorView in the contentView and change it's size to be 0.85 of the content's one.
+        colorView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        colorView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        colorView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.85).isActive = true
+        colorView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.85).isActive = true
+
+        // Apply cornerRadius so as to make the view become a circle.
+        colorView.layer.cornerRadius = (frame.size.width * 0.85) / 2
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
     }
 }
