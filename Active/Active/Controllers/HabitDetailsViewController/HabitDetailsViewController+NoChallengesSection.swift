34cd0086e72043975309821a0186c61201c8aa67
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-08-22 14:13:00 -0300

    Add no challenge section to details scene

---
+++ Active/Active/Controllers/HabitDetailsViewController/HabitDetailsViewController+NoChallengesSection.swift
@@ -0,0 +1,16 @@
+//
+//  HabitDetailsViewController+NoChallengesSection.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 22/08/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import UIKit
+
+/// Adds code to display the section informing that there's no active days' challenge for the habit.
+extension HabitDetailsViewController {
+
+    // MARK: Imperatives
+
+}
