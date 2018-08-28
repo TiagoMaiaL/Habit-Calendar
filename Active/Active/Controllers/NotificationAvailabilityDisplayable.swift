6e034aaa59a4430e58fbe670d92e9f2fd3100406
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-08-28 13:46:13 -0300

    Add protocol to show notifications auth status
    This protocol declares the main methods needed to inform the user
    that user notifications aren't authorized. Any controller
    responsible for scheduling user notifications should adopt it.

---
+++ Active/Active/Controllers/NotificationAvailabilityDisplayable.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationAvailabilityDisplayable.swift
+//  Active
+//
+//  Created by Tiago Maia Lopes on 28/08/18.
+//  Copyright © 2018 Tiago Maia Lopes. All rights reserved.
+//
+
+import Foundation
+
+/// Adds the interface for displaying whether user notifications aren't allowed by the user.
+protocol NotificationAvailabilityDisplayable {
+
+    // MARK: Imperatives
+
+    /// The notification manager used to get the authorization status.
+    var notificationManager: UserNotificationManager! { get set }
+
+    // MARK: Imperatives
+
+    /// Registers an observer for the UIApplicationDidBecomeActive event.
+    /// - Note: This observer is used to make sure the view always displays the correct
+    ///         UserNotification auth status (The user goes to Settings app and comes back).
+    func observeForegroundEvent()
+
+    /// Unregisters any observer.
+    func removeObserver()
+
+    /// Gets the authorization status from the notification manager.
+    func getAuthStatus(_ notification: NSNotification?)
+
+    /// Displays information about the local user notifications authorization status.
+    func displayNotificationAvailability(_ isAuthorized: Bool)
+}
+
+/// Adds default implementations to some of the protocol's methods.
+extension NotificationAvailabilityDisplayable {
+
+    // MARK: Imperatives
+
+    func removeObserver() {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
