1594a7e5ec8cf86dd55c97ba077c7dcb229f0a4a
Tiago Maia <tiago.maia.lopes@gmail.com>
2018-10-22 13:24:34 -0300

    Add interaction animations to button

--- Habit-Calendar/Habit-Calendar/Views/RoundedButton.swift
+++ Habit-Calendar/Habit-Calendar/Views/RoundedButton.swift
@@ -36,4 +36,35 @@ import UIKit
         layer.cornerRadius = frame.size.height / 2
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        animateBeginOfTouch()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        animateEndOfTouch()
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        animateEndOfTouch()
+    }
+
+    // MARK: Imperatives
+
+    /// Animates the button to display the touch down event.
+    private func animateBeginOfTouch() {
+        UIViewPropertyAnimator(duration: 0.1, curve: .easeIn) {
+            self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }.startAnimation()
+    }
+
+    /// Animates the button to display the touch up event.
+    private func animateEndOfTouch() {
+        UIViewPropertyAnimator(duration: 0.5, dampingRatio: 0.3) {
+            self.transform = CGAffineTransform.identity
+            self.layer.cornerRadius = self.frame.size.height / 2
+        }.startAnimation()
+    }
 }
